f6e383597561f9d380cda7c10922ae4fd2d60907
wrp1002 <wes.hamster@gmail.com>
2019-09-27 14:38:14 +1200

    @alabasdean @hoefsldyla

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+var bodyParser = require("body-parser");
+var controller = require('./controllers/index.js');
 
-express()
+const PORT = process.env.PORT || 5000;
+
+app = express();
+
+controller(app);
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.json())
+  .use(function (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      // // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      // Request headers you wish to allow ,
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-ControlAllow-Headers');
+      // Pass to next layer of middleware
+      next();
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  //app.use(express.static('./public'));
+  
+  
