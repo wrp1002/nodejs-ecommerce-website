6c291efbf5e0ebeadccacc3bb419624e3fad06d6
Deanne Alabastro <aladeanra@gmail.com>
2019-09-29 00:10:50 +1200

    implement form validation

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,4 +1,64 @@
 $(document).ready(function () {
+    const $signupForm = $('#signupForm');
+    const $signupBtn = $('#signupbtn');
+    const $emailField = $('#email');
+    const $passwordField = $('#password');
+    const $confirmField = $('#confirmPassword');
+    let showError = true;
+
+    /**
+     * Validation for the login form
+     */
+    $signupForm.validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true
+            },
+            confirmPassword: {
+                required: true,
+                equalTo: "#password"
+            }
+        },
+        messages: {
+            email: {
+                required: "Please enter your email address",
+                email: "A valid email address is required"
+            },
+            password: {
+                required: "Please enter your password",
+            },
+            confirmPassword: {
+                required: "Please re-enter password",
+                equalTo: "Passwords must match"
+            }
+        },
+        showErrors: function (errorMap, errorList) {
+            if (showError) {
+                this.defaultShowErrors();
+            }
+        }
+    }).checkForm();
+
+
+    /**
+     * Disables login button until all fields are valid
+     */
+    $signupForm.on("blur keyup change", "input", () => {
+        showError = false;
+
+        if ($emailField.valid() && $passwordField.valid()
+        && $confirmField.valid()) {
+            $signupBtn.removeAttr("disabled");
+        } else {
+            $signupBtn.attr("disabled", "disabled");
+        }
+        showError = true;
+    });
+    
     $('#signupbtn').click(function (event) {
 
       event.preventDefault();
