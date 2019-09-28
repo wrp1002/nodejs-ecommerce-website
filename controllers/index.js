08bb096822172c48a7cc440b60024d754c91607e
Dylan Hoefsloot <dylan.hoefsloot@outlook.com>
2019-09-28 15:51:30 +1200

    Minor updates to test login on heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const passwordHash = require('../passwordHash');
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 module.exports = function(app) {  
 
@@ -15,15 +16,53 @@ module.exports = function(app) {
     });
 
     app.get('/login', async (req, res) => {
-        
         res.render('pages/login');
+    });
+
+    app.post('/login', async (req, res) => {
+        
+        if(!req.body.email) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'Email is required'
+            });
+        }
+    
+        if(!req.body.password) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'Password is required'
+            });
+        }
+
+        //const validPassword = passwordHash.validatePassword(req.body.email, req.body.password)
+        
+        var validPassword = true;
+
+        if(validPassword){
+            
+            jwt.sign(req.body.email, "secretkey", { expiresIn: '15m' }, (err, token) => {
+                return res.status(201).send({
+                    success: 'true',
+                    message: 'User login successful',
+                    token: token
+                })
+            })
+        }
+        else {
+            return res.status(400).send({
+                success: 'false',
+                message: 'User login unsuccessful'
+            })
+        }
+
     });
 
     app.get('/register', async (req, res) => {
         res.render('pages/register');
     });
 
-    app.post('/register/submit', async (req, res) => {        
+    app.post('/register', async (req, res) => {        
         
         if(!req.body.email) {
             return res.status(400).send({
@@ -75,16 +114,45 @@ module.exports = function(app) {
         //res.render('pages/index');
     });
 
-    app.get('/account', async (req, res) => {
-        // purchase history and other info
-        
-        //res.render('pages/index');
+    app.get('/account', processToken, async (req, res) => {
+
+        jwt.verify(req.token, 'secretkey', (errorMessage, authData) => {
+            
+            if(errorMessage){
+                //res.render('pages/login');
+                return res.status(403).send({
+                    success: 'false',
+                    message: 'Error verifying authorization token'
+                })
+            }
+
+            // Purchase history and other info
+            res.render('pages/account');
+
+        })        
     });
 
     app.get('/cart', async (req, res) => {
         
         //res.render('pages/index');
     });
+}
 
+function processToken(req, res, next) {
+    
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+        const bearerArray = bearerHeader.split(' ');
+        const accessToken = bearerArray[1];
+        req.token = accessToken;
+        next();
+    }
+    else {
+        return res.status(403).send({
+            success: 'false',
+            message: 'Authorization token not found'
+        })
+    }
 }
 
