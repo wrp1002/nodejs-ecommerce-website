2f2e6a1e3792026cf43b517d7852cae6231fa088
Deanne Alabastro <aladeanra@gmail.com>
2019-10-02 21:39:29 +1300

    add post route for forgot password with server side email validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,25 +2,26 @@ const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const router = require('express').Router()
 const pg = require('pg');
-const {check, validationResult} = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+var crypto = require('crypto');
 
 const databasePool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 
-router.post('/register', async(req, res) => {
+router.post('/register', async (req, res) => {
 
     try {
 
         var errorList = [];
-        const{ email, name, password, confirm } = req.body;
-        
+        const { email, name, password, confirm } = req.body;
+
         if (!name || !email || !password || !confirm) errorList.push({ msg: 'Please enter all fields' });
         if (password != confirm) errorList.push({ msg: 'Passwords do not match' });
         if (password.length < 6) errorList.push({ msg: 'Password must be at least 6 characters' });
-      
+
         if (errorList.length > 0) {
 
             return res.render('pages/register', {
@@ -31,14 +32,14 @@ router.post('/register', async(req, res) => {
                 confirm
             });
         }
-        
-        const client = await databasePool.connect();
-                
-        await client.query("SELECT * FROM users WHERE email = $1", [email], function(errorMessage, userInformation) {
-                
-            if(errorMessage) throw errorMessage
 
-            if(userInformation.rowCount != 0){
+        const client = await databasePool.connect();
+
+        await client.query("SELECT * FROM users WHERE email = $1", [email], function (errorMessage, userInformation) {
+
+            if (errorMessage) throw errorMessage
+
+            if (userInformation.rowCount != 0) {
 
                 errorList.push({ msg: 'Email already exists' });
                 return res.render('pages/register', {
@@ -51,39 +52,39 @@ router.post('/register', async(req, res) => {
             }
 
             bcrypt.genSalt(10, (saltError, generatedSalt) => {
-                
-                if(saltError) throw saltError
 
-                bcrypt.hash(password, generatedSalt, async(hashError, passwordHash) => {
-                    
-                    if(hashError) throw hashError
-                    
-                    await client.query("INSERT INTO users (email, password_hash, salt) VALUES ($1, $2, $3)", [email, String(passwordHash), generatedSalt], function(errorMessage, results) {
-            
-                        if(errorMessage) throw errorMessage
+                if (saltError) throw saltError
+
+                bcrypt.hash(password, generatedSalt, async (hashError, passwordHash) => {
+
+                    if (hashError) throw hashError
+
+                    await client.query("INSERT INTO users (email, password_hash, salt) VALUES ($1, $2, $3)", [email, String(passwordHash), generatedSalt], function (errorMessage, results) {
+
+                        if (errorMessage) throw errorMessage
 
                         req.flash(
                             'success_msg',
                             'You are now registered and can log in'
                         );
-    
+
                     })
 
                 })
 
             })
-           
+
         })
 
-    } catch(Error) { console.error(String(Error)) }
+    } catch (Error) { console.error(String(Error)) }
 
 })
 
 router.get('/google', passport.authenticate('google', {
-    scope: [ 'profile', 'email' ]
+    scope: ['profile', 'email']
 }))
 
-router.get('/google/callback', async(req, res, next) => {
+router.get('/google/callback', async (req, res, next) => {
 
     passport.authenticate('google', {
 
@@ -95,8 +96,8 @@ router.get('/google/callback', async(req, res, next) => {
 
 })
 
-router.post('/login', async(req, res, next) => {
-    
+router.post('/login', async (req, res, next) => {
+
     passport.authenticate('local', {
 
         successRedirect: '/',
@@ -106,19 +107,26 @@ router.post('/login', async(req, res, next) => {
     })(req, res, next);
 
 })
-  
-router.get('/logout', async(req, res) => {
-    
+
+router.get('/logout', async (req, res) => {
+
     req.logout();
     req.flash('success_msg', 'You are logged out');
     res.redirect('/login');
 
 })
 
-router.post('/forgotpassword',[
-    check('email','Your email is not valid. Please enter a valid email address').not().isEmpty().isEmail().normalizeEmail()
-] ,async(req, res) => {
-    const{ email } = req.body;
+router.post('/forgotpassword', [
+    check('email', 'Your email is not valid. Please enter a valid email address').not().isEmpty().isEmail().normalizeEmail()
+], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const emailError = errors.mapped().email.msg;
+        // console.log("errors" , errors.mapped());
+        req.flash("error", emailError);
+        return res.redirect("/forgotpassword");
+    }
 
 })
 
