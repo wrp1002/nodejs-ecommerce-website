dde1de31f8b159b1248ca7bddd028ce263485e6d
Deanne Alabastro <aladeanra@gmail.com>
2019-10-26 22:36:18 +1300

    create folder for db interactions and index file for pool connect and query functions

---
+++ db/index.js
@@ -0,0 +1,33 @@
+const { Pool } = require('pg');
+
+/*
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+});
+*/
+
+// Used to local testing
+const pool = new Pool({
+    user: 'eqoaufryrlziba',
+    host: 'ec2-54-235-104-136.compute-1.amazonaws.com',
+    database: 'debldnvrsqnjov',
+    password: 'ca96d213b57dca84daf23d6c6e76840266b0aa26f73bbf30bff67f81d84002ff',
+    port: 5432,
+    ssl: true
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+        if (callback == null) {
+            return pool.query(text, params);
+        } else {
+            return pool.query(text, params, callback);
+        }
+    },
+    getClient: (callback) => {
+        pool.connect((err, client, done) => {
+            callback(err, client, done);
+        })
+    }
+}
