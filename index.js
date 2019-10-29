168f63973d6b5abe11a3bd8da018d75649e43e5c
Deanne Alabastro <aladeanra@gmail.com>
2019-10-29 21:47:06 +1300

    remove duplicate messages in res.locals

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ app.use(flash());
 
 // Global variables
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
   res.locals.success = req.flash('success');
   res.locals.info = req.flash('info');
   res.locals.error = req.flash('error');
